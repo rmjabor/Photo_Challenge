0c9ba8ae478a80896b70edacebf698cb447a201d
rmjabor <rafa@animavidens.com.br>
2021-05-07 15:10:35 -0300

    Creating messages.

---
+++ main.js
@@ -0,0 +1,25 @@
+const abstract = ['Take an abstract picture, ',
+                  'Take a concrete (non-abstract) picture, ',
+                  'Take a picture (abstract or concrete), '];
+const theme = ["theme 'portrait', ",
+               "theme 'landscape', ",
+               "theme 'plants', ",
+               "theme 'animals', ",
+               "theme 'odd objects', ",
+               "theme 'common objects', ",
+               "theme of your choice, "];
+const lighting = ['with natural light, ',
+                  'with artificial light, ',
+                  'combining natural and artificial light, ',
+                  'lighting of your choice, '];
+const contrast = ['high contrast, ',
+                  'low contrast, ',
+                  'contrast of your choice, '];
+const colors = ['colored ',
+                'in black-and-white ',
+                'duotoned ' ,
+                'colors of your choice '];
+const lens = ['and using a normal lens.',
+              'and using an angular lens.',
+              'and using a telephoto lens.',
+              'and using a lens of your choice.']
